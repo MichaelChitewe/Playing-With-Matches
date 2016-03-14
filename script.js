630c9d5f5667ff956cd069154c506b24c9d814ca
ignatius123 <ignatius@projectcodex.co>
2016-03-14 16:49:53 +0200

    Made changes to UX and displayed message

--- script.js
+++ script.js
@@ -1,188 +1,209 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+ function getRandomInt(min, max) {
+   return Math.floor(Math.random() * (max - min) + min);
+ }
 
-var numbers = document.querySelectorAll('.numbers');
+ var numbers = document.querySelectorAll('.numbers');
 
-function elements() {
-  for (var i = 0; i < numbers.length; i++) {
-    numbers[i].innerHTML = getRandomInt(1, 9);
-  }
-}
+ var messageDisplay = function(text) {
+   var message = document.getElementById("hippy").innerHTML = text;
+ }
 
-elements();
 
-// function upDate() {
-//   document.getElementsByClassName('numbers')[1].classList.add(
-//     'highlight');
-// document.getElementsByClassName('numbers')[2].classList.add(
-//   'highlight');
-// document.getElementsByClassName('numbers')[3].classList.add(
-//   'highlight');
-// // document.getElementsByClassName('numbers')[4].classList.add(
-//   'highlight');
-// document.getElementsByClassName('numbers')[5].classList.add(
-//   'highlight');
-// document.getElementsByClassName('numbers')[6].classList.add(
-//   'highlight');
-// document.getElementsByClassName('numbers')[7].classList.add(
-//   'highlight');
-// document.getElementsByClassName('numbers')[8].classList.add(
-//   'highlight');
-// document.getElementsByClassName('numbers')[9].classList.add(
-//   'highlight');
-//}
+ function elements() {
 
-var numbers = document.getElementsByClassName('numbers');
+   var numbers = document.getElementsByClassName('numbers');
+   var messages = [];
 
-for (var i = 0; i < numbers.length; i++) {
-  numbers[i].classList.remove('highlight');
-}
+   for (var i = 0; i < numbers.length; i++) {
+     numbers[i].classList.remove('highlight');
+   }
 
-if (numbers[0].innerHTML === numbers[1].innerHTML) {
-  numbers[0].classList.add("highlight");
-  numbers[1].classList.add("highlight");
-}
-if (numbers[1].innerHTML === numbers[2].innerHTML) {
-  numbers[1].classList.add("highlight");
-  numbers[2].classList.add("highlight");
-}
-if (numbers[0].innerHTML === numbers[2].innerHTML) {
-  numbers[0].classList.add("highlight");
-  numbers[2].classList.add("highlight");
-}
+   for (var i = 0; i < numbers.length; i++) {
+     numbers[i].innerHTML = getRandomInt(1, 9);
+   }
 
-//first row down matches
+   if (numbers[0].innerHTML === numbers[1].innerHTML) {
+     numbers[0].classList.add("highlight");
+     numbers[1].classList.add("highlight");
+     messages.push(numbers[0].innerHTML + " and " + numbers[1].innerHTML +
+       " first row down match ");
+   }
 
-if (numbers[3].innerHTML === numbers[4].innerHTML) {
-  numbers[3].classList.add("highlight");
-  numbers[4].classList.add("highlight");
-}
+   if (numbers[1].innerHTML === numbers[2].innerHTML) {
+     numbers[1].classList.add("highlight");
+     numbers[2].classList.add("highlight");
+     messages.push(numbers[1].innerHTML + " and " + numbers[2].innerHTML +
+       " first row down match ");
+   }
 
-if (numbers[4].innerHTML === numbers[5].innerHTML) {
-  numbers[4].classList.add("highlight");
-  numbers[5].classList.add("highlight");
-}
+   if (numbers[0].innerHTML === numbers[2].innerHTML) {
+     numbers[0].classList.add("highlight");
+     numbers[2].classList.add("highlight");
+     messages.push(numbers[0].innerHTML + " and " + numbers[2].innerHTML +
+       " first row down match ");
+   }
 
-if (numbers[3].innerHTML === numbers[5].innerHTML) {
-  numbers[3].classList.add("highlight");
-  numbers[5].classList.add("highlight");
-}
+   //first row down matches
+   if (numbers[3].innerHTML === numbers[4].innerHTML) {
+     numbers[3].classList.add("highlight");
+     numbers[4].classList.add("highlight");
+     messages.push(numbers[3].innerHTML + " and " + numbers[4].innerHTML +
+       " second middle row down match ");
+   }
 
-// second row down matches
+   if (numbers[4].innerHTML === numbers[5].innerHTML) {
+     numbers[4].classList.add("highlight");
+     numbers[5].classList.add("highlight");
+     messages.push(numbers[4].innerHTML + " and " + numbers[5].innerHTML +
+       " second middle row down match ");
+   }
 
-if (numbers[6].innerHTML === numbers[7].innerHTML) {
-  numbers[6].classList.add("highlight");
-  numbers[7].classList.add("highlight");
-}
+   if (numbers[3].innerHTML === numbers[5].innerHTML) {
+     numbers[3].classList.add("highlight");
+     numbers[5].classList.add("highlight");
+     messages.push(numbers[3].innerHTML + " and " + numbers[5].innerHTML +
+       " second middle row down match ");
+   }
 
-if (numbers[7].innerHTML === numbers[8].innerHTML) {
-  numbers[7].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
+   // second row down matches
+   if (numbers[6].innerHTML === numbers[7].innerHTML) {
+     numbers[6].classList.add("highlight");
+     numbers[7].classList.add("highlight");
+     messages.push(numbers[6].innerHTML + " and " + numbers[7].innerHTML +
+       " third row down match ");
+   }
 
-if (numbers[6].innerHTML === numbers[8].innerHTML) {
-  numbers[6].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
+   if (numbers[7].innerHTML === numbers[8].innerHTML) {
+     numbers[7].classList.add("highlight");
+     numbers[8].classList.add("highlight");
+     messages.push(numbers[7].innerHTML + " and " + numbers[8].innerHTML +
+       " third row down match ");
+   }
 
-//third row down matches
+   if (numbers[6].innerHTML === numbers[8].innerHTML) {
+     numbers[6].classList.add("highlight");
+     numbers[8].classList.add("highlight");
+     messages.push(numbers[6].innerHTML + " and " + numbers[8].innerHTML +
+       " third row down match ");
+   }
 
-if (numbers[0].innerHTML === numbers[3].innerHTML) {
-  numbers[0].classList.add("highlight");
-  numbers[3].classList.add("highlight");
-}
+   //third row down matches
+   if (numbers[0].innerHTML === numbers[3].innerHTML) {
+     numbers[0].classList.add("highlight");
+     numbers[3].classList.add("highlight");
+     messages.push(numbers[0].innerHTML + " and " + numbers[3].innerHTML +
+       " first column top match ");
+   }
 
-if (numbers[3].innerHTML === numbers[6].innerHTML) {
-  numbers[3].classList.add("highlight");
-  numbers[6].classList.add("highlight");
-}
+   if (numbers[3].innerHTML === numbers[6].innerHTML) {
+     numbers[3].classList.add("highlight");
+     numbers[6].classList.add("highlight");
+     messages.push(numbers[3].innerHTML + " and " + numbers[6].innerHTML +
+       " first column top match ");
+   }
 
-if (numbers[0].innerHTML === numbers[6].innerHTML) {
-  numbers[0].classList.add("highlight");
-  numbers[6].classList.add("highlight");
-}
+   if (numbers[0].innerHTML === numbers[6].innerHTML) {
+     numbers[0].classList.add("highlight");
+     numbers[6].classList.add("highlight");
+     messages.push(numbers[0].innerHTML + " and " + numbers[6].innerHTML +
+       " first column top match ");
+   }
 
-//first diagonal column
+   //first diagonal column
+   if (numbers[1].innerHTML === numbers[4].innerHTML) {
+     numbers[1].classList.add("highlight");
+     numbers[4].classList.add("highlight");
+     messages.push(numbers[1].innerHTML + " and " + numbers[4].innerHTML +
+       " second middle column match ");
+   }
 
-if (numbers[1].innerHTML === numbers[4].innerHTML) {
-  numbers[1].classList.add("highlight");
-  numbers[4].classList.add("highlight");
-}
+   if (numbers[4].innerHTML === numbers[7].innerHTML) {
+     numbers[4].classList.add("highlight");
+     numbers[7].classList.add("highlight");
+     messages.push(numbers[4].innerHTML + " and " + numbers[7].innerHTML +
+       " second middle column match ");
+   }
 
-if (numbers[4].innerHTML === numbers[7].innerHTML) {
-  numbers[4].classList.add("highlight");
-  numbers[7].classList.add("highlight");
-}
+   if (numbers[1].innerHTML === numbers[7].innerHTML) {
+     numbers[1].classList.add("highlight");
+     numbers[7].classList.add("highlight");
+     messages.push(numbers[1].innerHTML + " and " + numbers[7].innerHTML +
+       " second middle column match ");
+   }
 
-if (numbers[1].innerHTML === numbers[7].innerHTML) {
-  numbers[1].classList.add("highlight");
-  numbers[7].classList.add("highlight");
-}
+   //second diagonal column
+   if (numbers[2].innerHTML === numbers[5].innerHTML) {
+     numbers[2].classList.add("highlight");
+     numbers[5].classList.add("highlight");
+     messages.push(numbers[2].innerHTML + " and " + numbers[5].innerHTML +
+       " third bottom column match ");
+   }
 
-//second diagonal column
+   if (numbers[5].innerHTML === numbers[8].innerHTML) {
+     numbers[5].classList.add("highlight");
+     numbers[8].classList.add("highlight");
+     messages.push(numbers[5].innerHTML + " and " + numbers[8].innerHTML +
+       " third bottom column match ");
+   }
 
-if (numbers[2].innerHTML === numbers[5].innerHTML) {
-  numbers[2].classList.add("highlight");
-  numbers[5].classList.add("highlight");
-}
-if (numbers[5].innerHTML === numbers[8].innerHTML) {
-  numbers[5].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
+   if (numbers[2].innerHTML === numbers[8].innerHTML) {
+     numbers[2].classList.add("highlight");
+     numbers[8].classList.add("highlight");
+     messages.push(numbers[2].innerHTML + " and " + numbers[8].innerHTML +
+       " third bottom column match ");
+   }
 
-if (numbers[2].innerHTML === numbers[8].innerHTML) {
-  numbers[2].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
+   //first diagonal row
+   if (numbers[6].innerHTML === numbers[4].innerHTML) {
+     numbers[6].classList.add("highlight");
+     numbers[4].classList.add("highlight");
+     messages.push(numbers[6].innerHTML + " and " + numbers[4].innerHTML +
+       " first diagonal row match ");
+   }
 
-//third diagonal column
+   if (numbers[4].innerHTML === numbers[2].innerHTML) {
+     numbers[4].classList.add("highlight");
+     numbers[2].classList.add("highlight");
+     messages.push(numbers[4].innerHTML + " and " + numbers[2].innerHTML +
+       " first diagonal row match ");
+   }
 
-if (numbers[0].innerHTML === numbers[4].innerHTML) {
-  numbers[0].classList.add("highlight");
-  numbers[4].classList.add("highlight");
-}
+   if (numbers[6].innerHTML === numbers[2].innerHTML) {
+     numbers[6].classList.add("highlight");
+     numbers[2].classList.add("highlight");
+     messages.push(numbers[6].innerHTML + " and " + numbers[2].innerHTML +
+       " first diagonal row match ");
+   }
 
-if (numbers[4].innerHTML === numbers[8].innerHTML) {
-  numbers[4].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
+   //second diagonal row
+   if (numbers[0].innerHTML === numbers[4].innerHTML) {
+     numbers[0].classList.add("highlight");
+     numbers[4].classList.add("highlight");
+     messages.push(numbers[0].innerHTML + " and " + numbers[4].innerHTML +
+       " second diagonal row match ");
+   }
 
-if (numbers[0].innerHTML === numbers[8].innerHTML) {
-  numbers[0].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
+   if (numbers[4].innerHTML === numbers[8].innerHTML) {
+     numbers[4].classList.add("highlight");
+     numbers[8].classList.add("highlight");
+     messages.push(numbers[4].innerHTML + " and " + numbers[8].innerHTML +
+       " second diagonal row match ");
+   }
 
-//first diagonal row
+   if (numbers[0].innerHTML === numbers[8].innerHTML) {
+     numbers[0].classList.add("highlight");
+     numbers[8].classList.add("highlight");
+     messages.push(numbers[0].innerHTML + " and " + numbers[8].innerHTML +
+       " second diagonal row match ");
 
-if (numbers[6].innerHTML === numbers[4].innerHTML) {
-  numbers[6].classList.add("highlight");
-  numbers[4].classList.add("highlight");
-}
+   }
 
-if (numbers[4].innerHTML === numbers[2].innerHTML) {
-  numbers[4].classList.add("highlight");
-  numbers[2].classList.add("highlight");
-}
+   var msg = messages.join("<br>");
+   return msg;
+ }
 
-if (numbers[6].innerHTML === numbers[2].innerHTML) {
-  numbers[6].classList.add("highlight");
-  numbers[2].classList.add("highlight");
-}
+ function btn() {
+   messageDisplay(elements());
+ }
 
-//second diagonal row
-
-if (numbers[0].innerHTML === numbers[4].innerHTML) {
-  numbers[0].classList.add("highlight");
-  numbers[4].classList.add("highlight");
-}
-
-if (numbers[4].innerHTML === numbers[8].innerHTML) {
-  numbers[4].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
-
-if (numbers[0].innerHTML === numbers[8].innerHTML) {
-  numbers[0].classList.add("highlight");
-  numbers[8].classList.add("highlight");
-}
+ elements();
